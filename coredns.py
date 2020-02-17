2b2eba4992d3ef970cde002466ff4dce332cf491
QiuDog <qiudog825@gmail.com>
2020-02-18 00:08:07 +0800

    added coroutine to get domain from coredns log

--- coredns.py
+++ coredns.py
@@ -13,22 +13,26 @@ class CoreDNS():
         try:
             self.__client = docker.DockerClient(base_url=docker_url)
             self.__coredns = self.__client.containers.get(container)
-            self.logger = self.__coredns.logs(follow=True, stream=True, tail=0)
+            self.__logging = self.__coredns.logs(follow=True, stream=True, tail=0)
         except Exception as e:
             logging.error('Docker error')
             logging.error(e)
 
     def domains(self):
-        for line in self.logger:
+        for line in self.__logging:
             line = line.decode('utf-8')
             if 'NOERROR' in line:
                 domain = line.split()[-1][:-1]
                 if '.' in domain:
                     yield domain
 
+    async def a_get_domain(self):
+        return next(self.domains())
+
 if __name__ == '__main__':
     from dotenv import load_dotenv
     import os, sys, signal
+    import asyncio
     load_dotenv()
 
     def signal_handler(signal, frame):
@@ -36,5 +40,9 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
 
     coreDNS = CoreDNS(docker_url=os.getenv('DOCKER_ADDR'), container='coredns')
-    for d in coreDNS.domains():
-        print(d)
+    loop = asyncio.get_event_loop()
+    cr = coreDNS.a_get_domain()
+    task = asyncio.ensure_future(cr)
+    loop.run_until_complete(task)
+
+    print(task.result())
